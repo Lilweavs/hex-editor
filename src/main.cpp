b5688b82b98c6fdc3a51346fca4708ae324e724f
LilWeavs <andrewjw827@gmail.com>
2023-10-07 17:11:39 -0500

    oops

--- src/main.cpp
+++ src/main.cpp
@@ -493,6 +493,3 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-// Copyright 2020 Arthur Sonzogni. All rights reserved.
-// Use of this source code is governed by the MIT license that can be found in
-// the LICENSE file.
